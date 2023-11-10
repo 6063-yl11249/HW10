2b2f73af4d8682d6a2c5d324f6a2be5a0e560b87
Yuanqing <yl11249@nyu.edu>
2023-11-10 16:28:13 -0500

    time design

--- HW10.ino
+++ HW10.ino
@@ -8,57 +8,56 @@ void setup() {
 }
 
 void loop() {
-    for (int pin = 2; pin <= 7; pin++) {
-  // LED on pin 2 blinks for the first 10 seconds
-  if (millis() <= 9000) {
-    blinkLED(2);
-  } else {
-    digitalWrite(2, HIGH);
+  unsigned long startTime = millis();
+
+  while (millis() - startTime < 60000) {
+    if (millis() - startTime <= 9000) {
+      blinkLED(2);
+    } else {
+      digitalWrite(2, HIGH);
+    }
+
+    if (millis() - startTime >= 10000 && millis() - startTime <= 19000) {
+      blinkLED(3);
+    } else if (millis() - startTime > 19000) {
+      digitalWrite(3, HIGH);
+    }
+
+    if (millis() - startTime >= 20000 && millis() - startTime <= 29000) {
+      blinkLED(4);
+    } else if (millis() - startTime > 29000) {
+      digitalWrite(4, HIGH);
+    }
+
+    if (millis() - startTime >= 30000 && millis() - startTime <= 39000) {
+      blinkLED(5);
+    } else if (millis() - startTime > 39000) {
+      digitalWrite(5, HIGH);
+    }
+
+    if (millis() - startTime >= 40000 && millis() - startTime <= 49000) {
+      blinkLED(6);
+    } else if (millis() - startTime > 49000) {
+      digitalWrite(6, HIGH);
+    }
+
+    if (millis() - startTime >= 50000 && millis() - startTime <= 59000) {
+      blinkLED(7);
+    } else if (millis() - startTime > 59000) {
+      digitalWrite(7, HIGH);
+    }
+
+    delay(15);
   }
 
-  // LED on pin 3 blinks for the second 10 seconds
-  if (millis() >= 10000 && millis() <= 19000) {
-    blinkLED(3);
-  } else if (millis() > 19000) {
-    digitalWrite(3, HIGH);
+  for (int pin = 2; pin <= 7; pin++) {
+    digitalWrite(pin, LOW);
   }
-
-  // LED on pin 4 blinks for the third 10 seconds
-  if (millis() >= 20000 && millis() <= 29000) {
-    blinkLED(4);
-  } else if (millis() > 29000) {
-    digitalWrite(4, HIGH);
-  }
-
-  // LED on pin 5 blinks for the fourth 10 seconds
-  if (millis() >= 30000 && millis() <= 39000) {
-    blinkLED(5);
-  } else if (millis() > 39000) {
-    digitalWrite(5, HIGH);
-  }
-
-  // LED on pin 6 blinks for the fifth 10 seconds
-  if (millis() >= 40000 && millis() <= 49000) {
-    blinkLED(6);
-  } else if (millis() > 49000) {
-    digitalWrite(6, HIGH);
-  }
-
-  // LED on pin 7 blinks for the sixth 10 seconds
-  if (millis() >= 50000 && millis() <= 59000) {
-    blinkLED(7);
-  } else if (millis() > 59000) {
-    digitalWrite(7, HIGH);
-  }
-   
-  delay (15);
-  digitalWrite(2, LOW);
-  } 
 }
 
 void blinkLED(int pin) {
   digitalWrite(pin, HIGH);
-  delay(500); 
+  delay(500);
   digitalWrite(pin, LOW);
-  delay(500); 
+  delay(500);
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# HW10
+ 1. Description of how you encoded seconds into lights.  
+ I set up six LED lights where each LED represents 10 seconds. Each LED blinks every second, and after 10 seconds, it stays on to show that 10 seconds have passed. When all six LEDs are on, it means 60 seconds have gone by. The sequence then resets and starts again, repeating every 60 seconds.  
+
+ 2. A schematic drawing of your circuit.  
+ ![circuit](drawing.jpg)
  
